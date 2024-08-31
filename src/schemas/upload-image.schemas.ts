9e47db81627020bd0fe706f3e6640300a34f764f
Kadu <kadu.silva2014@gmail.com>
2024-08-30 22:27:09 -0300

    🚀 feat: add validation for base64 encoded image in upload-image schema
      📄 Detailed description of the commit:
    
        - Added a refine function to the image field in the bodySchema to validate if the provided string is a valid base64 encoded image.
        - Modified the transform function to remove the 'data:image/*;base64,' prefix from the image string before validation.

--- src/schemas/upload-image.schemas.ts
+++ src/schemas/upload-image.schemas.ts
@@ -2,7 +2,19 @@ import z from 'zod'
 
 export const bodySchema = z
   .object({
-    image: z.string().base64(),
+    image: z
+      .string()
+      .refine((image) => {
+        try {
+          Buffer.from(image, 'base64')
+          return true
+        } catch (error) {
+          return false
+        }
+      })
+      .transform((image) =>
+        image.replace(/^data:image\/[^;]+;base64,[^\s]+/, ''),
+      ),
     customer_code: z.string(),
     measure_datetime: z.coerce.date(),
     measure_type: z.enum(['WATER', 'GAS']),
